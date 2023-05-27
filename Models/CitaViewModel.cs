b3cddb551577e4b551287bed718ab4ca18ede9ff
guasonsad <nikole2019@hotmail.com>
2023-05-27 02:22:13 -0500

    Model Consulta

---
+++ Models/CitaViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Ciudadanos_Sanos.Models
+{
+	public class CitaViewModel
+	{
+		public DateTime Fecha { get; set; }
+		public string NombrePaciente { get; set; }
+		public string NombreMedico { get; set; }
+	}
+}
