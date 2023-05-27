f81684b3a22dd21a3eed444d7d11f982d82a8095
guasonsad <nikole2019@hotmail.com>
2023-05-27 02:33:47 -0500

    Models Usuario

---
+++ Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+namespace Ciudadanos_Sano.Models
+{
+	public class Usuario
+	{
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdUsuario { get; set; }
+		public string Correo { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
+		[Display(Name = "Contraseña Encriptada")]
+		public string Clave { get; set; }
+
+
+	}
+}
+
